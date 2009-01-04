40bc785b1513b0ea2ff90e650120ce5d896e930f
koexistens <koexistens@027a4cba-da5e-11dd-8772-d1b041bf0e37>
2009-01-04 22:47:21 +0000

    The server now uses UDP instead of TCP.

--- src/elasticity/server.py
+++ src/elasticity/server.py
@@ -1,27 +1,16 @@
-
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-
-from twisted.internet import reactor, protocol
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 
 
-class ElasticityServerProtocol(protocol.Protocol):
-    """This is just about the simplest possible protocol"""
-    
-    def dataReceived(self, data):
-        "As soon as any data is received, write it back."
-        self.transport.write(data)
+class ElasticityServerProtocol(DatagramProtocol):
+    def datagramReceived(self, datagram, address):
+        self.transport.write(datagram, address)
 
 
 def main():
-    """This runs the protocol on port 8000"""
-    factory = protocol.ServerFactory()
-    factory.protocol = ElasticityServerProtocol
-    reactor.listenTCP(8000,factory)
+    reactor.listenUDP(8000, ElasticityServerProtocol())
     reactor.run()
 
 
-# this only runs if the module was *not* imported
 if __name__ == '__main__':
     main()
