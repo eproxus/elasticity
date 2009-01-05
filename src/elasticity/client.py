b293089fa110de94f2b06560dde8f6ff3bec9f57
koexistens <koexistens@027a4cba-da5e-11dd-8772-d1b041bf0e37>
2009-01-05 22:59:04 +0000

    Added a net module. Start first the server then the client. With a press of space on the client it connects to the server. Further presses will send a package which the server bounces. The class is ported from C++ from this article: http://gafferongames.wordpress.com/networking-for-game-programmers/virtual-connection-over-udp/

--- src/elasticity/client.py
+++ src/elasticity/client.py
@@ -1,6 +1,3 @@
-# System imports.
-import socket
-
 # Pygame imports.
 import pygame
 from pygame.locals import *
@@ -9,19 +6,26 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+# Elasticity imports.
+import net
+
+
+# Connection parameters.
+serverPort = 30000
+clientPort = 30001
+protocolId = "head"
+deltaTime = 0.25
+deltaTimeInMS = 250
+timeOut = 10.0
+
 
 def main():
-    # Server address and receive buffer size (auto sized for sending).
-    serverAddress = ("localhost", 50010)
-    bufferSize = 1024
-    
-    # Create socket and bind it (bind is only needed for receiving data).
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        clientSocket.bind(("", 50005))
-    except:
-        print "Could not bind socket."
-    clientSocket.setblocking(False)
+    # Init connections.
+    connection = net.Connection(protocolId, timeOut)    
+    if not connection.start(clientPort):
+        print "Could not start connection on port " + str(clientPort)
+    connection.connect(("127.0.0.1", serverPort))
+    connected = False
     
     # Init pygame.
     width = 800
@@ -49,33 +53,40 @@ def main():
         # Get events.
         for event in pygame.event.get():
             if event.type == QUIT:
-                clientSocket.close()
+                del connection
                 return
             if event.type == KEYUP and event.key == K_ESCAPE:
-                clientSocket.close()
+                del connection
                 return
             if event.type == KEYUP and event.key == K_SPACE:
-                try:
-                    clientSocket.sendto('space', serverAddress)
-                except:
-                    print "Could not send data."
+                data = "Client to server"
+                connection.sendPacket(data, len(data))
 
         # Advance time.
-        time_passed = clock.tick()
-        time_passed_seconds = time_passed / 1000.
+        msPassed = clock.tick()
+        secondsPassed = msPassed / 1000.0
         
-        # Receive server data.
-        try:
-            data, addr = clientSocket.recvfrom(bufferSize)
-            if data:
-                print "Data received: " + data + ", from: " + addr
-        except:
-            #print "No data received."
-            pass
+        # Communicate with server
+        if not connected and connection.isConnected():
+            print "Client connected to server"
+            connected = True
+        if not connected and connection.connectFailed():
+            print "Connection failed"
+            del connection
+            return
+        #data = "Client to server"
+        #connection.sendPacket(data, len(data))
+        while True:
+            dataReceived = connection.receivePacket(256)
+            if not dataReceived:
+                break
+            print "Received packet from server"
+        
+        # Update the connection.
+        connection.update(secondsPassed)
 
         # Render scene.
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
         
         # Finish rendering by showing the back buffer.
         pygame.display.flip()
--- src/elasticity/server.py
+++ src/elasticity/server.py
@@ -1,50 +1,50 @@
-# System imports.
-import socket
+# Pygame imports.
+import pygame
+from pygame.locals import *
+
+# Elasticity imports.
+import net
+
+
+# Connection parameters.
+serverPort = 30000
+clientPort = 30001
+protocolId = "head"
+deltaTime = 0.25
+deltaTimeInMS = 250
+timeOut = 10.0
 
 
 def main():
-    # Client address and receive buffer size (auto sized for sending).
-    bufferSize = 1024
+    # Init connections.
+    connection = net.Connection(protocolId, timeOut)    
+    if not connection.start(serverPort):
+        print "Could not start connection on port " + str(serverPort)
+    connection.listen()
     
-    # Create a server socket used for sending data.
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serverSocket.setblocking(False)
-    
-    # Create one socket for each client and bind it to receive data.
-    clientSocket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSocket1.bind(("localhost", 50008))
-    clientSocket1.setblocking(False)
-    
-    # Add all clients to a list.
-    clientSockets = [clientSocket1]
+    # Init the clock.
+    clock = pygame.time.Clock()
     
     # Receive messages.
     while True:
-        data = ''
-        address = ''
-        
-        # Try to receive from all clients.
-        for clientSocket in clientSockets:
-            try:
-                data, address = clientSocket.recvfrom(bufferSize)
-            except:
-                #print "No data received."
-                pass
-        
-        if data:
-            print "Data received: " + data
-            try:
-                serverSocket.sendto(data, address)
-                print "Data sent: " + data
-            except:
-                print "Could not send data."
-    
-    # Close client sockets.
-    for clientSocket in clientSockets:
-        clientSocket.close()
+        # Advance time.
+        msPassed = clock.tick()
+        secondsPassed = msPassed / 1000.0
             
-    # Close server sockets.
-    serverSocket.close()
+        dataReceived = connection.receivePacket(256)
+        if dataReceived:
+            print "Received packet from client"
+            if connection.isConnected():
+                data = "Server to client"
+                connection.sendPacket(data, len(data))
+        
+        # Update the connection.
+        connection.update(secondsPassed)
+        
+        pygame.time.wait(deltaTimeInMS)
+    
+    # Close the connection.
+    del connection
 
 
 if __name__ == '__main__':
