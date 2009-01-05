99678d01e2f6808313bdf8606cdec6cb72ffd51c
koexistens <koexistens@027a4cba-da5e-11dd-8772-d1b041bf0e37>
2009-01-05 13:41:52 +0000

    Twisted removed, it now uses only python UDP sockets. Simple echo of the received data for now.

--- src/elasticity/server.py
+++ src/elasticity/server.py
@@ -1,15 +1,50 @@
-from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor
-
-
-class ElasticityServerProtocol(DatagramProtocol):
-    def datagramReceived(self, datagram, address):
-        self.transport.write(datagram, address)
+# System imports.
+import socket
 
 
 def main():
-    reactor.listenUDP(8000, ElasticityServerProtocol())
-    reactor.run()
+    # Client address and receive buffer size (auto sized for sending).
+    bufferSize = 1024
+    
+    # Create a server socket used for sending data.
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSocket.setblocking(False)
+    
+    # Create one socket for each client and bind it to receive data.
+    clientSocket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSocket1.setblocking(False)
+    clientSocket1.bind(("localhost", 50008))
+    
+    # Add all clients to a list.
+    clientSockets = [clientSocket1]
+    
+    # Receive messages.
+    while True:
+        data = ''
+        address = ''
+        
+        # Try to receive from all clients.
+        for clientSocket in clientSockets:
+            try:
+                data, address = clientSocket.recvfrom(bufferSize)
+            except:
+                #print "No data received."
+                pass
+        
+        if data:
+            print "Data received: " + data
+            try:
+                serverSocket.sendto(data, address)
+                print "Data sent: " + data
+            except:
+                print "Could not send data."
+    
+    # Close client sockets.
+    for clientSocket in clientSockets:
+        clientSocket.close()
+            
+    # Close server sockets.
+    serverSocket.close()
 
 
 if __name__ == '__main__':
